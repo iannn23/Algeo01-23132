70dc779197200ae5cfa5942d2a3233e19eb22927
Nathan <therealnathanmc7383@gmail.com>
2024-10-10 15:06:47 +0700

    feat: added functions for inputting and receiving a matrix's value based on the index

--- src/Matrix.java
+++ src/Matrix.java
@@ -2,9 +2,19 @@ class Matrix {
     double[][] mat;
     int nRow, nCol;
     
+    //Matriks
     public Matrix(int row, int column) {
         nRow = row;
         nCol = column;
         mat = new double[nRow][nCol];
+    }
+
+    //Memasukkan nilai ke matriks
+    public void inputELMT(int i, int j, double val) {
+        this.mat[i][j] = val;
+    }
+    //Mendapatkan nilai indeks matriks
+    public double retrieveELMT(int i, int j) {
+        return this.mat[i][j];
     }
 }
