dfdaafa51107fda30b6f863a3836670613f3e5e4
Nathan <therealnathanmc7383@gmail.com>
2024-10-10 15:45:50 +0700

    feat: added transpose of matrix

--- src/Matrix.java
+++ src/Matrix.java
@@ -17,4 +17,15 @@ class Matrix {
     public double retrieveELMT(int i, int j) {
         return this.mat[i][j];
     }
+
+    //Transpose matriks
+    public Matrix transpose() {
+        Matrix mT = new Matrix(nRow, nCol);
+        for (int i=0; i<nRow; i++) {
+            for (int j=0; j<nCol; j++) {
+                mT.mat[j][i] = mat[i][j];
+            }
+        }
+        return mT;
+    }
 }
