c107294cc405fd1fa36dbfe61ae91708f2ba3317
Nathan <therealnathanmc7383@gmail.com>
2024-10-13 15:46:45 +0700

    feat: added determinant, fixed transpose function

--- src/Matrix.java
+++ src/Matrix.java
@@ -43,12 +43,45 @@ class Matrix {
 
     //Transpose matriks
     public Matrix transpose() {
-        Matrix mT = new Matrix(nRow, nCol);
+        Matrix mT = new Matrix(nCol, nRow);
         for (int i=0; i<nRow; i++) {
             for (int j=0; j<nCol; j++) {
                 mT.mat[j][i] = mat[i][j];
             }
         }
         return mT;
+    }
+
+    public double determinant() {
+        if (nRow==nCol) {
+            if (nRow==1) return retrieveELMT(0, 0);
+            
+            double det = 0.0;
+            int plusMinus = 1;
+            for (int j=0; j<=nCol-1; j++) {
+                //m[0][j] = a_1j
+                if (retrieveELMT(0,j)!=0) {
+                    Matrix subMat = new Matrix(nRow-1, nCol-1);
+                    //submatrix M_ij
+                    int iSub = 0;
+                    for (int i=1; i<=nRow-1; i++) {
+                        int jSub = 0;
+                        for (int k=0; k<=nCol-1; k++) {
+                            if (k!=j) {
+                                subMat.inputELMT(iSub, jSub, retrieveELMT(i, k));
+                                jSub++;
+                            }
+                        }
+                        iSub++;
+                    }
+                    //Determinant = ((-1)^(i+j)) * a_ij * |M_ij|
+                    //note: i & j starts from 1; but in code starts from 0
+                    det += plusMinus * retrieveELMT(0, j) * subMat.determinant(); //rekursif
+                }
+                plusMinus *= -1;
+            }
+            return det;
+        }
+        return 0;
     }
 }
